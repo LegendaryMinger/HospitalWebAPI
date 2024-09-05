7738c3d0dc3a1b706d794e28ecad806b7bcf7087
ElovikovStepan <selovikov12345@gmail.com>
2024-09-05 19:50:28 +0500

    Создание контекста для получения данных

---
+++ HospitalWebAPI/Contexts/HospitalContext.cs
@@ -0,0 +1,27 @@
+﻿using HospitalWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalWebAPI.Contexts
+{
+	public class HospitalContext : DbContext
+	{
+		public DbSet<Appointment> Appointment { get; set; }
+		public DbSet<AppointmentDisease> AppointmentDisease { get; set; }
+		public DbSet<Comment> Comment { get; set; }
+		public DbSet<Department> Department { get; set; }
+		public DbSet<Disease> Disease { get; set; }
+		public DbSet<Employee> Employee { get; set; }
+		public DbSet<Equipment> Equipment { get; set; }
+		public DbSet<Gender> Gender { get; set; }
+		public DbSet<History> History { get; set; }
+		public DbSet<Instruction> Instruction { get; set; }
+		public DbSet<Patient> Patient { get; set; }
+		public DbSet<Payment> Payment { get; set; }
+		public DbSet<Service> Service { get; set; }
+		public HospitalContext() { }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseMySql("server=127.0.0.1;uid=root;pwd=root;database=hospital", new MySqlServerVersion(new Version(8, 0, 11)));
+		}
+	}
+}
---
+++ HospitalWebAPI/Controllers/DataRetrievalController.cs
@@ -0,0 +1,28 @@
+﻿using HospitalWebAPI.Contexts;
+using HospitalWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalWebAPI.Controllers
+{
+	[Route("WebAPI/DataRetrievalController")]
+	public class DataRetrievalController : Controller
+	{
+		[Route("Gender")]
+		[HttpGet]
+		public ActionResult GenderList()
+		{
+			try
+			{
+				using (var context = new HospitalContext())
+				{
+					IEnumerable<Gender> GenderList = context.Gender.ToList();
+					return Json(GenderList);
+				}
+			}
+			catch
+			{
+				return StatusCode(500);
+			}
+		}
+	}
+}
